a96c84570a3eac1b426e716094b0d4defeff7774
Zane Sandin <zanesandin@gmail.com>
2021-11-14 21:01:21 -0500

    Fixes javascript progress.

--- app/javascript/chat_rooms.js
+++ app/javascript/chat_rooms.js
@@ -28,20 +28,18 @@ document.addEventListener("DOMContentLoaded", function() {
   var current_chatroom_div = document.getElementById("chat_room_json_data_url");
   var messages_ul = document.getElementById("chat_room_message_list")
   var message_form = document.getElementById("message_form");
-  if (current_chatroom_div && messages_ul && message_form) {
-    function submitChatroomMessage(event) {
-      event.preventDefault();
-      new_mesage = new Message(event.content)
-      console.log("New message content is " + JSON.stringify(new_mesage.content));
-    }
-    console.log("all elements loaded");
-    message_form.addEventListener("submit", submitChatroomMessage);
+  // These will be refatored to backend stuff eventually when I know what I'm doing
+  var sender_id = document.getElementById("sender_id");
+  var participant_ids = document.getElementById("participant_ids");
+  var chat_room_id = document.getElementById("chat_room_id");
+  if (current_chatroom_div && messages_ul && message_form && sender_id && participant_ids && chat_room_id) {
+    var csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
     const CURRENT_CHATROOM_URL = current_chatroom_div.innerHTML;
     let Message = class {
       constructor(message) {
         this.content = message.content
         this.sender_id = message.sender_id
-        this.recipient_id = message.recipient_id
+        this.recipient_ids = message.recipient_ids
       }
 
       static sendNewMessage() {
@@ -59,15 +57,16 @@ document.addEventListener("DOMContentLoaded", function() {
         fetch(CURRENT_CHATROOM_URL)
         .then(res => res.json())
         .then(chatroomData => {
+          var participant_data = {sender_id: chatroomData["user_id"], participant_ids: chatroomData['participant_ids']}
+          return participant_data
         });
       }
 
-      static getChatRoomMessages() {
+      static setChatRoomMessages() {
         fetch(CURRENT_CHATROOM_URL)
         .then(res => res.json())
         .then(chatroomData => {
           chatroomData['messages'].forEach((message_data) => {
-            console.log("Message data is " + JSON.stringify(message_data))
             new_li = document.createElement("li");
             sent_at_date = new Date(message_data['created_at'])
             new_li.innerHTML = "<hr>" +  message_data['content'] + "<br>" + "Sent at: " + sent_at_date + "<hr>"
@@ -76,7 +75,32 @@ document.addEventListener("DOMContentLoaded", function() {
         })
       }
     }
-    ChatRoom.getChatRoomMessages();
-    console.log("Current chatroom div present");
+    ChatRoom.setChatRoomMessages();
+    function submitChatroomMessage(event) {
+      var message_form = document.getElementById("message_form");
+      var message_content = message_form.content.value;
+      //const new_message = new Message(message_form.content.value, sender_id.innerHTML, participant_ids.innerHTML)
+      //console.log("New message content is " + new_message.content)
+      fetch("/chat_rooms/" + chat_room_id.innerHTML + "/messages", {
+        method: "POST",
+        headers: {
+          "X-CSRF-Token": csrfToken,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(
+          {content: message_content, chat_room_id: chat_room_id.innerHTML, sender_id: sender_id.innerHTML}
+        )
+      }).then(response => {
+        if (!response.ok) { throw response; }
+        return response.json();
+      }).then((data) => {
+        console.log(data);
+      }).catch(error => {
+        console.error("error", error);
+      });
+
+      event.preventDefault();
+    }
+    message_form.addEventListener("submit", submitChatroomMessage);
   }
 });
